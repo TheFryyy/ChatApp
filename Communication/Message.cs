f3e26d4d1011172511d18ebbd6ff9e795baa72ac
TheFryyy <47969114+TheFryyy@users.noreply.github.com>
2021-02-03 13:16:01 +0100

    Upload Project

---
+++ Communication/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication
+{
+    // ensemble des types de messages pouvant être envoyé.
+    public enum MessageType { signin, login, logout, createTopic, connectTopic, sendMessage, getTopics, username, rcvMessage, broadcast, privateMsg, listUsers, disconnectTopic };
+    [Serializable]
+    public class Message
+    {
+        //Un message est composé d'un type de message et d'un texte qui sera traité en fonction de son type.
+        // pour envoyer plusieurs info on sépare par un élément et on utilise la fonction Split().
+        private MessageType type;
+        private string msg;
+
+        public Message(MessageType type, string msg)
+        {
+            this.Type = type;
+            this.Msg = msg;
+        }
+
+        
+        public string Msg { get => msg; set => msg = value; }
+        public MessageType Type { get => type; set => type = value; }
+    }
+    
+}
---
+++ Communication/Net.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Communication
+{
+    public class Net // s'occupe de sérialiser et désérialiser les messages (moyen de communication entre client et serveur.
+    {
+
+        public static void sendMessage(Stream s, Message msg)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(s, msg);
+        }
+
+        public static Message rcvMessage(Stream s)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            return (Message)bf.Deserialize(s);
+        }
+
+    }
+}
